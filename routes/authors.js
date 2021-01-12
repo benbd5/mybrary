bfe8d1438fd2d3a80923ad5ecca5a0009264e358
benbd5 <belzbenjaminp@gmail.com>
2021-01-12 11:11:47 +0100

    show and edit authors by id

--- routes/authors.js
+++ routes/authors.js
@@ -34,14 +34,52 @@ router.post("/", async (req, res) => {
   });
   try {
     const newAuthor = await author.save();
-    // res.redirect(`authors/${newAuhtor.id}`);
-    res.redirect("authors");
+    res.redirect(`authors/${newAuhtor.id}`);
   } catch {
     res.render("authors/new", {
       author: author,
       errorMessage: "Error creating Author", // quand input est vide
     });
   }
+});
+
+// Authors: show/edit/delete
+router.get("/:id", (req, res) => {
+  res.send("Show Author " + req.params.id);
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id);
+    res.render("authors/edit", {
+      author: author,
+    });
+  } catch {
+    res.redirect("/authors");
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  let author;
+  try {
+    const author = await Author.findById(req.params.id);
+    author.name = req.body.name;
+    await author.save();
+    res.redirect(`/authors/${author.id}`);
+  } catch {
+    if (author == null) {
+      res.redirect("/");
+    } else {
+      res.render("authors/edit", {
+        author: author,
+        errorMessage: "Error updating Author",
+      });
+    }
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("Delete Author " + req.params.id);
 });
 
 module.exports = router;
